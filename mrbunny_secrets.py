ed816c708a781ea5781b03deec9fee22bc71dd2a
Koushik Tummepalli <koushikt.cbook@gmail.com>
2025-11-03 21:27:45 -0800

    Update mrbunny_secrets.py

--- mrbunny_secrets.py
+++ mrbunny_secrets.py
@@ -1,3 +1,4 @@
 # secrets.py
 OPENROUTER_API_KEY = "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"
 GOOGLE_CLIENT_ID = "785298027158-ku3p3uhsnapaemk9np011cmptnrcjea8.apps.googleusercontent.com"
+GOOGLE_CLIENT_SECRET = ""
