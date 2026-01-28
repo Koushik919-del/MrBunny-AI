c05a93bd2d57e94a87fe7d3bea593e3d1971b835
Koushik Tummepalli <koushikt.cbook@gmail.com>
2026-01-27 19:05:07 -0800

    Update mrbunny_secrets.py

--- mrbunny_secrets.py
+++ mrbunny_secrets.py
@@ -1,4 +1,4 @@
 # secrets.py
 OPENROUTER_API_KEY = "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"
-GOOGLE_CLIENT_ID = "785298027158-ku3p3uhsnapaemk9np011cmptnrcjea8.apps.googleusercontent.com"
-GOOGLE_CLIENT_SECRET = "GOCSPX-8aCCa3lln07PCe4fHLq1HpypRwj"
+GOOGLE_CLIENT_ID = "785298027158-capm4nn64c4o1v42fi20mej6vm53asdq.apps.googleusercontent.com"
+GOOGLE_CLIENT_SECRET = "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"
